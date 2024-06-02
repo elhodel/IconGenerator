64aeab17f2219c5180f5abac5ed27e4b4f1f100b
elhodel <>
2024-06-02 14:47:16 +0200

    Check if TMP Essential exisits
    Also require TMP as Dependency

--- Editor/IconGeneratorBuildProcessor.cs
+++ Editor/IconGeneratorBuildProcessor.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using TMPro;
+
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
@@ -21,6 +23,27 @@ namespace elhodel.IconGenerator
             if (!IconGeneratorSettings.instance.GenerateIcon)
             {
                 return;
+            }
+
+            if (!TextMeshProExists())
+            {
+                int result = EditorUtility.DisplayDialogComplex("TextMeshPro missing for Icon Generator!", "TextMeshPro Essentials are missing but are required for the Icon Generator to work. \n\n Should it be imported now?", "Import now (Cancels the Build)", "Don't generate Icon this time", "Disable Icon Generator");
+
+                if (result == 0)
+                {
+                    TMP_PackageUtilities.ImportProjectResourcesMenu();
+                    throw new BuildFailedException("Icon Generator needs TextMeshPro Essentials . Import the Essentials now.");
+                }
+                else if (result == 1)
+                {
+                    return;
+                }
+                else
+                {
+                    IconGeneratorSettings.instance.GenerateIcon = false;
+                    return;
+                }
+
             }
 
             IconGeneratorSettings settings = IconGeneratorSettings.instance;
@@ -59,6 +82,14 @@ namespace elhodel.IconGenerator
             GameObject.DestroyImmediate(spawned);
 
 
+        }
+
+        private static bool TextMeshProExists()
+        {
+            /// Do the same check as TMPro makes in <see cref="TMP_Settings.instance"/>
+            /// Don't check against <see cref="TMP_Settings.instance"/> because the Editor window should not be opened in case the instance is null
+            TMP_Settings tmpSettings = Resources.Load<TMP_Settings>("TMP Settings");
+            return tmpSettings != null;
         }
 
         public static string GetRelativePath(string fullPath, string basePath, bool useBackwardSlashes = false, bool keepFileExtension = true)
